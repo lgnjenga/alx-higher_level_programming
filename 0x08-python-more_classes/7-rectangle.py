5bcc927d357b098113fa6e1928f368ce4887bc53
lgnjenga <lgnjenga@gmail.com>
2023-09-06 15:02:53 -0700

    7. Change representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -3,15 +3,15 @@
 
 
 class Rectangle:
-    """Represents a rectangle.
+    """Represent a rectangle.
 
     Attributes:
-        num_instances (int): The number of Rectangle instances.
-        symbol (str): The symbol used for string representation.
+        number_of_instances (int): The number of Rectangle instances.
+        print_symbol (any): The symbol used for string representation.
     """
 
-    num_instances = 0
-    symbol = "#"
+    number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
@@ -20,52 +20,109 @@ class Rectangle:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
-        self._width = 0
-        self._height = 0
-        self.set_dimensions(width, height)
+        type(self).number_of_instances += 1
+        self.width = width
+        self.height = height
 
-    def set_dimensions(self, width, height):
-        """Set the dimensions of the rectangle.
+    @property
+    def width(self):
+        """Get/set the width of the Rectangle."""
+        return self.__width
 
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
-        if not isinstance(width, int) or not isinstance(height, int):
-            raise TypeError("Width and height must be integers")
-        if width < 0 or height < 0:
-            raise ValueError("Width and height must be >= 0")
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        self._width = width
-        self._height = height
+    @property
+    def height(self):
+        """Get/set the height of the Rectangle."""
+        return self.__height
 
-    def calculate_area(self):
-        """Calculate and return the area of the Rectangle."""
-        return self._width * self._height
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
-    def calculate_perimeter(self):
-        """Calculate and return the perimeter of the Rectangle."""
-        return 2 * (self._width + self._height)
+    def area(self):
+        """Return the area of the Rectangle."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the perimeter of the Rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
 
     def display(self):
         """Display the Rectangle as a grid of symbols."""
-        if self._width == 0 or self._height == 0:
+        if self.__width == 0 or self.__height == 0:
             return ""
 
         rect = []
-        for _ in range(self._height):
-            rect.append(str(self.symbol) * self._width)
+        for _ in range(self.__height):
+            rect.append(str(self.print_symbol) * self.__width)
+
+        return "\n".join(rect)
+
+    def display_alternating(self, symbol1, symbol2):
+        """Display an alternating pattern of two symbols."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        rect = []
+        for i in range(self.__height):
+            symbol = symbol1 if i % 2 == 0 else symbol2
+            rect.append(str(symbol) * self.__width)
+
+        return "\n".join(rect)
+
+    def display_single(self, symbol):
+        """Display a single symbol pattern."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        return "\n".join([str(symbol)] * self.__height)
+
+    def display_multiple(self, symbols):
+        """Display a pattern of multiple symbols."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        rect = []
+        for _ in range(self.__height):
+            rect.extend([str(s) for s in symbols])
 
         return "\n".join(rect)
 
     def __str__(self):
-        """Return a string representation of the Rectangle."""
-        return self.display()
+        """Return the printable representation of the Rectangle.
+
+        Represents the rectangle with the # character.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        rect = []
+        for i in range(self.__height):
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return "".join(rect)
 
     def __repr__(self):
-        """Return a string representation of the Rectangle for debugging."""
-        return f"Rectangle({self._width}, {self._height})"
+        """Return the string representation of the Rectangle."""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return rect
 
     def __del__(self):
         """Print a message for every deletion of a Rectangle."""
-        print("Deleting a Rectangle...")
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
