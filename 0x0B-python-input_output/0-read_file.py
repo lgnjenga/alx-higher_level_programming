d6858b7d109e518e984c39206f675d60ebe8c57c
lgnjenga <lgnjenga@gmail.com>
2023-09-12 17:18:48 -0700

    0. A Python function that reads a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,49 +1,14 @@
 #!/usr/bin/python3
+"""Print the contents of a UTF8 text file to stdout."""
+
 
 def read_file(filename=""):
-    """
-    Reads a text file (UTF8) and prints its contents to stdout.
-
-    Args:
-        filename (str): The name of the text file to be read.
-        Defaults to an empty string.
-
-    Returns:
-        None
-
-    Raises:
-        None
-
-    Notes:
-        This function reads the specified text file
-        and prints its contents line by line
-        to the standard output (stdout).
-        It uses UTF-8 encoding to handle text files.
-
-    Example:
-        If you have a text file named "my_file.txt" with the following content:
-
-        ```
-        This is line 1
-        This is line 2
-        ```
-
-        You can use the `read_file` function to print its content as follows:
-
-        ```python
-        read_file("my_file.txt")
-        ```
-
-        Output:
-        ```
-        This is line 1
-        This is line 2
-        ```
-    """
-    with open(filename, encoding="utf-8") as file:
-        for line in file:
-            print(line, end="")
-
-
-if __name__ == "__main__":
-    read_file("my_file_0.txt")
+    try:
+        with open(filename, encoding="utf-8") as f:
+            while True:
+                chunk = f.read(1024)  # Read 1 KB at a time
+                if not chunk:
+                    break
+                print(chunk, end="")
+    except FileNotFoundError:
+        pass  # File doesn't exist, do nothing
