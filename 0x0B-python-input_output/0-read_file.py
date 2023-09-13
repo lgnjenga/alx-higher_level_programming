6d1495d029def65d534b058681746f005f7514bc
lgnjenga <lgnjenga@gmail.com>
2023-09-12 17:31:33 -0700

    0. A Python function that reads a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
-"""Print the contents of a UTF8 text file to stdout."""
+"""Defines a function that read a text file"""
 
 
 def read_file(filename=""):
-    """Print the contents of a text file"""
-    with open(filename, encoding="utf-8") as file_text:
-        print(file_text.read(), end="")
+    """Print the contents of a UTF8 text file to stdout."""
+    try:
+        with open(filename, encoding="utf-8") as f:
+            while True:
+                chunk = f.read(1024)  # Read 1 KB at a time
+                if not chunk:
+                    break
+                print(chunk, end="")
+    except FileNotFoundError:
+        pass  # File doesn't exist, do nothing
+
+# Test the function
+if __name__ == "__main__":
+    read_file("my_file_0.txt")
