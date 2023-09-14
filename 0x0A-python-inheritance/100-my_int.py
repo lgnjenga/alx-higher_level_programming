0005fa8db73480046fd0ccc44816175082e2c97d
lgnjenga <lgnjenga@gmail.com>
2023-09-13 19:55:52 -0700

    Task 12 & 13

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Defines the MyInt class."""
+
+
+class MyInt(int):
+    """A class that inherits from int and inverts == and != operators."""
+
+    def __eq__(self, other):
+        """Override the equality (==) operator."""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Override the inequality (!=) operator."""
+        return super().__eq__(other)
--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Defines the add_attribute function."""
+
+
+def add_attribute(obj, attr_name, attr_value):
+    """Add a new attribute to an object if possible."""
+    if hasattr(obj, '__dict__'):
+        setattr(obj, attr_name, attr_value)
+    else:
+        raise TypeError("can't add new attribute")
