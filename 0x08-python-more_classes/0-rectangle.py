ba020190d1e89e3fe5ca13212a0d0197e504deb7
lgnjenga <lgnjenga@gmail.com>
2023-10-31 01:08:54 +0300

    Task 0 2nd correction, adding class definition above class itself

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Definition of a Rectangle Class"""
+
+
 class Rectangle:
     """
-    This is a simple Rectangle class.
-    It defines a rectangle with no attributes or methods.
+    This is a simple represenation of a rectangle.
     """
     pass
