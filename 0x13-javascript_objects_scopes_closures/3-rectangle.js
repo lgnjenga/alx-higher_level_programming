eb2e3c5183366bdceeba0983441b9499650ce1b8
lgnjenga <lgnjenga@gmail.com>
2024-01-11 03:47:20 +0300

    4 & 5: Javascript Class Methods

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // Create an empty object if w or h is not a positive integer or equal to 0
+      Object.defineProperty(this, 'width', {
+        enumerable: false,
+        writable: true
+      });
+
+      Object.defineProperty(this, 'height', {
+        enumerable: false,
+        writable: true
+      });
+    }
+  }
+
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,42 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // Create an empty object if w or h is not a positive integer or equal to 0
+      Object.defineProperty(this, 'width', {
+        enumerable: false,
+        writable: true
+      });
+
+      Object.defineProperty(this, 'height', {
+        enumerable: false,
+        writable: true
+      });
+    }
+  }
+
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
